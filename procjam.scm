50c733604b1e10ed9103ed669fad1f49bc23efac
Kooda <kooda@upyum.com>
2017-11-11 16:31:39 +0100

    Flower hybridization

--- procjam.scm
+++ procjam.scm
@@ -9,7 +9,6 @@
 (load "utils.scm")
 
 (define base-color (random-color))
-(print (color->cairo base-color))
 
 (define ww/3 (round (/ ww 3)))
 (define ww/2 (round (/ ww 2)))
@@ -38,13 +37,12 @@
                               (color:rotate-hue comp (- a))
                               (color:rotate-hue comp (* 2 (- a)))))))
          )
-  `((center-color . ,center-color)
-    (center-size . ,(pseudo-random-real))
-    (poly . ,poly)
-    (layers . ,layers)
-    (angle . ,(* 2pi (pseudo-random-real)))
-    (petals ,@(map random-petal-layer colors))
-    )))
+    (list center-color                     ;; center color
+          (pseudo-random-real)             ;; center size
+          poly                             ;; polygon vertices number
+          (* 2pi (pseudo-random-real))     ;; skew
+          (map random-petal-layer colors)) ;; petals layers
+    ))
 
 (define (random-petal-layer color)
   (let* ((angle-div (between 1 5))
@@ -68,10 +66,66 @@
           (* 2pi (pseudo-random-real)))
     ))
 
+(define (hybridize section f1 f2)
+  (let ()
+    (map
+      (lambda (i g1 g2)
+        (cond ((color? g1)
+               (apply color:L*C*h
+                      (hybridize (pseudo-random-integer 3)
+                                 (color->L*C*h g1)
+                                 (color->L*C*h g2))))
+              ((pair? g1)
+               (let ((l1 (length g1))
+                     (l2 (length g2)))
+                 (cond ((> l1 l2)
+                        (hybridize section
+                                   g1
+                                   (if (zero? (pseudo-random-integer 2))
+                                       g2
+                                       (append g2 g1)))
+                        )
+                       ((> l2 l1)
+                        (hybridize section
+                                   (if (zero? (pseudo-random-integer 2))
+                                       g1
+                                       (append g1 g2))
+                                   g2)
+                        )
+                       (else
+                         (hybridize section g1 g2)))))
+              ((> i section)
+               g1)
+              (else
+                g2))
+        )
+      (iota (length f1)) f1 f2)
+      ))
+
+(define (all-hybridizations f1 f2)
+  (list-tabulate 7 (lambda (i) (hybridize i f1 f2))))
+
 #;(set-pseudo-random-seed! (random-bytes))
 (set-pseudo-random-seed! #${3c7ed09c3a86bb8c35cb8a35ee5a30a5e1fbc23b928c9596e605b54abc9dc09b3bf4d8feac0c97df97ec8f0602c3dc48cf6170d92a7485333a6798f1d1f2eef1a492a16a639141011ce1ac45923f0a83c962985d656e78292b12e96ee888185967151127bcd3f419d450a5e2e9921b0ebb9b7ae9e6076446fa13beb32d7420bf})
 (define *flowers*
-  (list-tabulate 40 (lambda (i) (cons i (random-flower)))))
+  (list-tabulate 40 (lambda (i) (random-flower))))
+
+(pp (car *flowers*))
+
+(define fl1 (list-ref *flowers* 3))
+(define fl2 (list-ref *flowers* 25))
+(define fl3 (list-ref *flowers* 30))
+
+
+(define fl1-fl2 (all-hybridizations fl1 fl2))
+(define fl2-fl3 (all-hybridizations fl2 fl3))
+(define fl1-fl3 (all-hybridizations fl1 fl3))
+
+(define *flowers*
+  (append (list fl1) fl1-fl2 (list fl2) fl2-fl3 (list fl3) 
+          (reverse fl1-fl3) (list fl1)))
+
+(pp (car *flowers*))
 
 (define (show-frame)
   (set! base-color (color:rotate-hue base-color (/ dt 10)))
@@ -88,38 +142,49 @@
   
   ;; 40 flowers
   (for-each
-    (lambda (i+f)
+    (lambda (i f)
       (save! ctx)
-      (let* ((i (car i+f))
-             (f (cdr i+f))
-             (x (modulo i 10))
+      (let* ((x (modulo i 10))
              (y (quotient i 10)))
         (translate! ctx
                     (+ (/ ww 20) (* x (/ ww 10)))
                     (+ (/ wh 8) (* y (/ wh 4))))
-        (scale! ctx 1.5 1.5)
-        #;(rotate! ctx (alist-ref 'angle f))
-        (flower! f))
-    (restore! ctx))
+        (scale! ctx 20 20)
+        (apply flower! f))
+      (restore! ctx))
+    (iota (length *flowers*))
     *flowers*)
   
   ;; Flower number n
-  #;(let ((n 11))
-      (save! ctx)
-      (translate! ctx ww/2 wh/2)
-      (scale! ctx 10 10)
-      (flower! (cdr (list-ref *flowers* n)))
-      (restore! ctx))
+  #;(let ((n 14))
+    (save! ctx)
+    (translate! ctx ww/2 wh/2)
+    (scale! ctx 100 100)
+    (apply flower! (list-ref *flowers* n))
+    (restore! ctx))
   
   ;; Stream of flowers
   #;(let ()
-      (save! ctx)
-      (translate! ctx ww/2 wh/2)
-      (scale! ctx 10 10)
-      (flower! (random-flower))
-      (restore! ctx)
-      (thread-sleep! 0.5))
+    (save! ctx)
+    (translate! ctx ww/2 wh/2)
+    (scale! ctx 100 100)
+    (apply flower! (random-flower))
+    (restore! ctx)
+    (thread-sleep! 0.5))
 
+  ;; hybridation test
+  #;(let* ((fls (cons* fl1 fl2 fl3 (append fl1-fl2 fl2-fl3 fl1-fl3)))
+         (len (length fls)))
+    (for-each
+      (lambda (i f)
+        (save! ctx)
+        (translate! ctx (inexact (* i (/ ww (add1 len)))) wh/2)
+        (scale! ctx 10 10)
+        (apply flower! f)
+        (restore! ctx))
+      (iota len 1)
+      fls))
+  
   )
 
 (define (color-line! colors)
@@ -133,24 +198,19 @@
     colors)
   (restore! ctx))
 
-(define (flower! fl)
-  (let ((poly (alist-ref 'poly fl))
-        (layers (alist-ref 'layers fl))
-        (pts (alist-ref 'petals fl)))
-    (scale! ctx 10 10)
-    
-    (for-each
-      (lambda (p)
-        (apply petals poly p)
-        (rotate! ctx (/ 2pi (* poly layers))))
-      pts)
-    
-    (center (alist-ref 'center-size fl)
-            (alist-ref 'center-color fl)
-            poly)
-  ))
+(define (flower! center-color center-size poly skew layers)
+  (save! ctx)
+  (rotate! ctx skew)
+  (for-each
+    (lambda (p)
+      (apply petals! poly p)
+      (rotate! ctx (/ 2pi (* poly (length layers)))))
+    layers)
+  (center! center-size center-color poly)
+  (restore! ctx)
+  )
   
-(define (center size color n)
+(define (center! size color n)
   (let ((angle (/ 2pi n))
         (base (make-polar size 0))
         (c (color->L*C*h color)))
@@ -167,7 +227,7 @@
              )
     ))
 
-(define (petals n color . args)
+(define (petals! n color . args)
   (let ((p (pattern-create-radial 0 0 0 0 0 2)))
     (apply pattern-add-color-stop-rgb!
            p 0.1 (color->cairo (color:scale-chroma color 0.3)))
@@ -178,12 +238,13 @@
     (dotimes (i n)
       (rotate! ctx (/ 2pi n))
       (apply petal i args))
+    (fill! ctx)
     (pattern-destroy! p)))
 
 (define (petal i near far near-angle far-angle moment)
   (let* ((v (make-polar (+ 2
-                           (* 1/100 (sin (* (add1 i) moment))))
-                        (* 1/100 (cos (* (add1 i) moment)))))
+                           (* 1/50 (sin (* (add1 i) moment))))
+                        (* 1/50 (cos (* (add1 i) moment)))))
          (v2 (make-polar (* (magnitude v) near)
                          (+ (angle v) near-angle)))
          (v3 (make-polar (* (magnitude v) far)
@@ -195,5 +256,4 @@
     (move-to! ctx 0 0)
     (vec-curve-to! ctx v2 v3 v)
     (vec-curve-to! ctx v5 v4 0)
-    (fill! ctx)
     ))
